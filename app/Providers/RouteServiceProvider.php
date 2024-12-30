eac087cf977709fe95f523a95f25c31360e39c29
Pritam Kumar Kundu <pritam.cse2014@gmail.com>
2024-12-30 12:52:51 +0600

    Admin-And-User-Login-Part-2

---
+++ app/Providers/RouteServiceProvider.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\ServiceProvider;
+
+class RouteServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+
+    public const HOME = '/dashboard';
+    
+    public function boot(): void
+    {
+        $this->configureRateLimiting();
+
+        Route::middleware('api')
+            ->prefix('api')
+            ->group(base_path('routes/api.php'));
+
+        Route::middleware('web')
+            ->group(base_path('routes/web.php'));
+    }
+
+    protected function configureRateLimiting(): void
+    {
+        RateLimiter::for('api', function (Request $request) {
+            return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
+        });
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,25 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::middleware('admin:admin')->group(function () {
+    Route::get('admin/login', [AdminController::class, 'adminLoginAdd']);
+    Route::post('admin/login', [AdminController::class, 'adminLoginStore'])->name('admin.login');
+});
+
+Route::middleware([
+    'auth:sanctum,admin',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/admin/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
 });
 
 Route::middleware([
